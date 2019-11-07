302481c33ce1f08ebcb0887b9f6f784981ad4847
vivian <viviankaun@gmail.com>
2019-11-07 11:10:40 -0800

    Update 21 Merge Two Sorted Lists.py

--- 21 Merge Two Sorted Lists.py
+++ 21 Merge Two Sorted Lists.py
@@ -5,20 +5,20 @@ class Solution(object):
         :type l2: ListNode
         :rtree: ListNode
         : Linked list 增加一個Dummy head,更改下節點
-        ç TimeComplexity : All other work is constant, so the overall complexity is linear.
+        ç TimeComplexity :  O(M+N) All other work is constant, so the overall complexity is linear.
         ç SpaceComplexity : O(1)
         解題 https://www.youtube.com/watch?v=GfRQvf7MB3k
         """
-        prehead=ListNode(-1)
-        prev=prehead
+        dummy=ListNode(-1)
+        cur=dummy
         while l1 and l2:
             if l1.val <= l2.val:
-                prev.next=l1
+                cur.next=l1
                 l1=l1.next
             else:
-                prev.next=l2
+                cur.next=l2
                 l2=l2.next
-            prev=prev.next
+            cur=cur.next
             
-        prev.next=l1 if l1 is not None else l2
-        return prehead.next
+        cur.next=l1 if l1 is not None else l2
+        return dummy.next 
