c6f0dfa75c74c625c579092249c72c717137fe54
vivian <viviankaun@gmail.com>
2020-05-05 00:01:06 -0700

    Delete .122 best-time-to-buy-and-sell-stock-ii.py

--- .122 best-time-to-buy-and-sell-stock-ii.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        pre_p = prices[0]
-        for p in prices[1:]:
-            if p > pre_p:
-                profit += p - pre_p
-            pre_p = p
-        return profit
