8f5e554869b157b2193870e7a2bad351e37b9e08
vivian <viviankaun@gmail.com>
2020-07-20 19:37:08 -0700

    Create 297SerializeandDeserializeBinaryTree.py

---
+++ 297SerializeandDeserializeBinaryTree.py
@@ -0,0 +1,43 @@
+class Codec:
+    from collections import deque
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root: return ''
+        queue = deque()
+        queue.append(root)
+        res=''
+        while queue:
+          node=queue.popleft()
+          if not node: 
+            res+='None,'
+            continue
+          res += str(node.val)+','
+          queue.append(node.left)
+          queue.append(node.right)
+        
+        return res 
+
+    def deserialize(self, data):
+        if not data: return None
+        ls = data.split(',')
+        root = TreeNode(int(ls[0]))
+        queue = collections.deque()
+        queue.append(root)
+        i = 1
+        while queue and i < len(ls):
+            node = queue.popleft()
+            if ls[i] != 'None':
+                left = TreeNode(int(ls[i]))
+                node.left = left
+                queue.append(left)
+            i += 1
+            if ls[i] != 'None':
+                right = TreeNode(int(ls[i]))
+                node.right = right
+                queue.append(right)
+            i += 1
+        return root
