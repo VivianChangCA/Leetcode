17f2072f276884d1495baa77ffdf001556e175ff
vivian <viviankaun@gmail.com>
2021-05-27 07:55:28 -0700

    Update 200 200. Number of Islands200. Number of Islands.py

--- 200 200. Number of Islands200. Number of Islands.py
+++ 200 200. Number of Islands200. Number of Islands.py
@@ -6,21 +6,20 @@ class Solution:
         n, m = len(grid), len(grid[0])
         
         def bfsHelper(i, j):
-            grid[i][j] = '0'
-            queue = []
-            queue.append((i,j))
-            
-            while queue:
-                x, y = queue.pop()
-                for (nx, ny) in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
-                    if (0 <= nx < n) and (0 <= ny < m):
-                        if grid[nx][ny]=='1':
+            grid[i][j]='0'
+            stack=[]
+            stack.append( (i,j))
+            while stack:
+              x,y = stack.pop()
+              for (nx, ny) in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]: 
+                     if (0 <= nx < n) and (0 <= ny < m) and grid[nx][ny]=='1':
                             grid[nx][ny]='0'
-                            queue.append((nx, ny))
+                            stack.append((nx, ny))
+                  
                 
         cnt = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j]=='1':
                     bfsHelper(i, j)
                     cnt += 1
