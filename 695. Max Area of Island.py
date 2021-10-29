7d74ced154edb02a54625132c4e19ed98b299754
vivian <viviankaun@gmail.com>
2021-10-28 17:59:16 -0700

    Update 695. Max Area of Island.py

--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -3,22 +3,18 @@ class Solution:
         row = len(grid)
         col = len(grid[0])
         self.size= 0 
-        
-        def dfs(x,y ):
-          grid[x][y] = 0
-          
-          for i, j in ([x+1,y], [x-1,y], [x,y+1], [x,y-1]) :
-            if 0 <= i < row and 0 <= j < col and grid[i][j] == 1 : 
-              self.size += 1 
+        def dfs(x,y ):  
+          grid[x][y] = 0 
+          self.size += 1  
+          for i, j in ([x+1,y], [x-1,y], [x,y+1], [x,y-1]) : 
+            if 0 <= i < row and 0 <= j < col and grid[i][j] == 1 :  # next cell is island 
               dfs (i,j) 
               
         res= 0 
         for i in range(row): 
           for j in range(col):
-            if grid[i][j] == 1 : 
-              self.size = 1
+            if grid[i][j] == 1 :  
+              self.size  = 0  # initialize 0 for each island  
               dfs(i, j) 
               res = max(res, self.size)
         return res 
-        
-        
