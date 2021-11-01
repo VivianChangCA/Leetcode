5cf06ee222877c79518b94acaedbb1e95c7537a0
vivian <viviankaun@gmail.com>
2021-11-01 16:31:53 -0700

    Create 695. Max Area of Island_2.py

---
+++ 695. Max Area of Island_2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        row = len(grid)
+        col = len(grid[0]) 
+        def dfs(x,y ):  
+          grid[x][y] = 0  
+          sumt= 0
+          for i, j in ([x+1,y], [x-1,y], [x,y+1], [x,y-1]) : 
+            if 0 <= i < row and 0 <= j < col and grid[i][j] == 1 :   
+              sumt += dfs (i,j) 
+          return 1 + sumt 
+              
+        res= 0 
+        for i in range(row): 
+          for j in range(col):
+            if grid[i][j] == 1 :  
+              res = max(res, dfs(i, j))
+        return res 
