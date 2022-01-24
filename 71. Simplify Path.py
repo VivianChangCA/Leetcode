e0b8f3103b58079707e53d6864bea336f2c5121b
vivian <viviankaun@gmail.com>
2022-01-24 02:18:57 -0800

    Create 71. Simplify Path.py

---
+++ 71. Simplify Path.py
@@ -0,0 +1,11 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+      stack =  [] 
+      for portion in path.split("/"):
+        if portion == ".." : 
+          if stack: stack.pop()
+        elif portion == "" or portion == ".": ## avoid // or . 
+          continue
+        else: 
+          stack.append(portion)  
+      return  "/"+"/".join(stack) 
