2dbb4fdc9122f8b178b5db300ee2df889e94dc49
vivian <viviankaun@gmail.com>
2022-01-27 17:17:37 -0800

    Update 269AlienDictionary.py
    for  break else 語法：沒有走到break 出來的，就走else

--- 269AlienDictionary.py
+++ 269AlienDictionary.py
@@ -1,28 +1,34 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        alien= defaultdict(list) 
-        indegree=  Counter({c : 0 for word in words for c in word}) 
-        for first, second in zip(words, words[1:]): 
-            for c1,c2 in zip(first,second):
-                if c1==c2: 
-                    continue
-                indegree[c2]+=1
-                alien[c1].append(c2) 
+        graph = defaultdict(list)
+        indegree = {c:0 for word in words for c in word}
+        
+        for w1,w2 in zip(words,words[1:]):
+            for c1, c2 in zip(w1,w2):
+                if c1 == c2: continue
+                indegree[c2] +=1
+                graph[c1].append( c2)
                 break
-            else:
-                if len(first)>len(second):
-                    return ""
-         
-        q=[i for i in indegree if indegree[i]==0] 
-        res= []
-        while q:
-            cur=q.pop(0)
-            res+=cur
-            for w in alien[cur]:
-                indegree[w]-=1
-                if indegree[w]==0:
-                    q.append(w)
-        if len(res)<len(indegree):
-            return ''
+            else: # w1= apples w2= apple , for 走完都沒有走到break,就跳到else
+                if len(w1) > len(w2): return ""
+        
+        q = [ c for c in indegree if indegree[c] == 0 ]
+        print(q,indegree)
+        
+        res =[]
+        while q: 
+            cur = q.pop()
+            res.append(cur)
+            for w in graph[cur]:
+                indegree[w] -= 1
+                if indegree[w] == 0 : q.append(w)
+                    
+        if len(res) < len(indegree):
+            return ""
         else:
             return "".join(res)
+                
+                
+                
+        
+        
