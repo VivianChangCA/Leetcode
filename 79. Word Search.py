37bc5258e94389275d88e524f36b7b405cc709c8
vivian <viviankaun@gmail.com>
2022-03-13 18:58:24 -0700

    Update 79. Word Search.py

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -2,8 +2,8 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         r = len(board)
         c = len(board[0])
-        self.visited = set()  
-        if len(word) > r * c: return False # case1
+        visited = set()  
+        if len(word) > r * c: return False 
         def isValid(i,j,idx):
             if idx ==0 : self.visited = set()  
             if (i,j) in self.visited: return False   
@@ -11,9 +11,9 @@ class Solution:
             if idx == len(word) - 1 and word[idx] == board[i][j]: return True 
             for x,y  in  [  (i+1,j), (i-1,j), (i,j+1), (i, j-1) ]:  
                 if x >= 0 and y >= 0 and x < r and y < c: 
-                    self.visited.add( (i,j) )  
+                    visited.add( (i,j) )  
                     if isValid(x,y,idx+1):  return True 
-            self.visited.remove((i,j))  # case2: 當前字是符合，可是擴展四個方向的鄰居後，沒有符合的。
+            visited.remove((i,j)) 
             return False  # 四個角都不成立時候會回傳 
         
         for i in range(r):
